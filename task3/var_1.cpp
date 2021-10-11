b309dfb79a9ece1e1692810554110a3d90e01fb4
sadchordoma <71036432+sadchordoma@users.noreply.github.com>
2021-10-11 16:27:12 +0500

    Сдал задачу 3 вариант 1

--- task3/var_1.cpp
+++ task3/var_1.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
-using namespace std;
-/**
+//1)/**
  * Дан отсортированный массив целых чисел A[0..n-1] и массив целых чисел B[0..m-1]. 
  * Для каждого элемента массива B[i] найдите минимальный индекс k минимального элемента массива A,
  * равного или превосходящего B[i]: A[k] >= B[i]. Если такого элемента нет, выведите n.
 
 n, m ≤ 10000.
  **/
+//2)	53789954
+#include <iostream>
+using namespace std;
+
 void input_data_from_console_(int len, int *mas) {   // compilator rugaetsya 4to func uje est' v projecte
     for (int i = 0; i < len; i++) {
         cin >> mas[i];
